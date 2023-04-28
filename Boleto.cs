b0720242041c8bbdafa8655a02fc474cf22cc66a
Gustavo Enzo Santos Aguilar <49816214857@info.local>
2023-04-28 17:10:58 -0300

    inicializada classe boleto

---
+++ Boleto.cs
@@ -0,0 +1,12 @@
+namespace projeto_pagamento
+{
+    public class Boleto
+    {
+        public string codigoBarras {get; private set;}
+
+        public void Registrar()
+        {
+            Console.WriteLine($"");
+        }
+    }
+}
